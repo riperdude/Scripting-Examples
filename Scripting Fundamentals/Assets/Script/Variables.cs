10947c744c9b62e12a3e062fc7342ed3bcf878bc
riperdude <146127014+riperdude@users.noreply.github.com>
2023-10-03 12:01:22 -0700

    update files

---
+++ Scripting Fundamentals/Assets/Script/Variables.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    //Public Member/Gobal Variables
+    public float MoveSpeed = 10f;
+    public int PlayerScore = 0;
+    public string PlayerName = "John Doe";
+    public bool IsPlayerActive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
