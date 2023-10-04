7ea7b187c6027d9a8ce8cf9c52011acb0a289a7d
riperdude <146127014+riperdude@users.noreply.github.com>
2023-10-04 11:37:26 -0700

    Update Variables

--- Scripting Fundamentals/Assets/Script/Variables.cs
+++ Scripting Fundamentals/Assets/Script/Variables.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class Variables : MonoBehaviour
 {
+    //Declaring Variables
     //Public Member/Gobal Variables
     public float MoveSpeed = 10f;
     public int PlayerScore = 0;
     public string PlayerName = "John Doe";
     public bool IsPlayerActive = true;
+
+    //Private Member/Gobol Variables
+    private float _maxSpeed = 100f;
+    private int _totalScore = 0;
+    private string _userName = "John255";
+    private bool _isGameOver = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +26,12 @@ public class Variables : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Declaring local variables
+        float horizontalInput = Input.GetAxis("Horizontal");
+    }
+    private void UpdateScore(int amount) 
+    {
+        // using a variable to update the total score
+    _totalScore += amount;
     }
 }
